62020ea79f12844eb17a6d428b6358617f410a32
Thiago Gualberto <thiago.gou@gmail.com>
2021-05-28 09:27:40 -0300

    Primeiros passos

---
+++ src/automate_processes/automate_processes.py
@@ -0,0 +1,14 @@
+import pyautogui
+import time
+import pyperclip
+
+pyautogui.PAUSE = 1
+#abrir navegador
+'''
+pyautogui.press("winleft")
+pyautogui.write("Google Chrome")
+pyautogui.press("enter")
+'''
+
+pyautogui.alert("Vai começar, aperte OK e não mexa em nada")
+pyautogui.hotkey("ctrl", "t")
