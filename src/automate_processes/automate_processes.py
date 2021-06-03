5909fe6ae947bec6cf0cc7dad94079c54765be1a
Thiago Gualberto <thiago.gou@gmail.com>
2021-06-03 08:24:55 -0300

    Fixing file

--- src/automate_processes/automate_processes.py
+++ src/automate_processes/automate_processes.py
@@ -12,3 +12,15 @@ pyautogui.press("enter")
 
 pyautogui.alert("Vai começar, aperte OK e não mexa em nada")
 pyautogui.hotkey("ctrl", "t")
+
+link="https://drive.google.com/drive/u/0/folders/1UWrsc-BnO6JMOReOzuyqEpMZ1uzU-Nsq"
+pyperclip.copy(link)
+pyautogui.hotkey("ctrl", "v")
+pyautogui.press("enter")
+time.sleep(15)
+
+pyautogui.position(317, 273, clicks=2)
+pyautogui.position(398, 274)
+pyautogui.position(1695, 170)
+pyautogui.position(1622, 616)
+time.sleep(10)
